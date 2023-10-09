d347bda26f0832b545d5a8d6f8142d1d405a40df
manpreet8021 <54615943+manpreet8021@users.noreply.github.com>
2023-10-09 17:39:59 +0530

    ui update

--- client-app/src/features/activities/dashboard/ActivityDetails.tsx
+++ client-app/src/features/activities/dashboard/ActivityDetails.tsx
@@ -28,7 +28,7 @@ export default observer(function ActivityDetails() {
                         <ActivityDetailChat />
                     </Grid.Column>
                     <Grid.Column width={6}>
-                        <ActivityDetailSidebar />
+                        <ActivityDetailSidebar attendees={activityStore.selectedActivity.attendees!}/>
                     </Grid.Column>
                 </Grid>
             }
